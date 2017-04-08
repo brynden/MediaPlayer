98ad1e830f7d58a55ded746fd43478b05b4291e0
brynden <Bryndenwilliams@yahoo.com>
2017-04-08 10:36:50 -0400

    source code
    source code

---
+++ MediaPlayerGUI.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mediaplayergui;
+
+import javafx.application.Application;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author brynd
+ */
+public class MediaPlayerGUI extends Application{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        
+        GridPane mainPane = new GridPane();
+        
+        
+    }
+    
+}
